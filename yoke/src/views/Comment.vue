5de851d2c5597c009e102db3ead7f4f718b8a456
zhaoyang110157 <1853935753@qq.com>
2019-07-05 10:52:45 +0800

    complete login interface

--- yoke/src/views/Comment.vue
+++ yoke/src/views/Comment.vue
@@ -1,7 +1,35 @@
 <template>
     <div>
         Comment Pages
+
+        <div id="lhs">
+            <div class="btype">期刊/报纸/外文原版</div>
+            <div class="btype">小说/文学/语言文字</div>
+            <div class="btype">历史/地理/艺术</div>
+            <div class="btype">政治/法律/军事</div>
+            <div class="btype">哲学/心理/宗教</div>
+            <div class="btype">经济/社科/综合</div>
+            <div class="btype">工程技术/互联网</div>
+            <div class="btype">自然科学/医药卫生</div>
+            <div class="btype">教材/教辅/考试</div>
+        </div>
+        <div id="rhs" >
+
+            <div id="box">
+
+                <img id="pic" src="../image/nuowei.jpg" width="333">
+                <div class="container">
+                    <h1 >挪威的森林（电影特别版）</h1>
+                    <br>  <h5>作者： 村上春树 &nbsp; 定价：35元</h5>
+                    <h5>ISBN: 9787532755387:</h5>
+                    <h6>内容简介:<br>
+                    </h6>
+                </div>
+            </div>
+
+        </div>
     </div>
+
 </template>
 
 <script>
